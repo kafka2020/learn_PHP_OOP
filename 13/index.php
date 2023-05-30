55e30a430090f1ad945a115fe93c7dc59762e59a
Андрей Шинкарев <shinkarev89@gmail.com>
2023-05-30 16:12:56 +0300

    Add 13 lesson Autoloading classes and Composer

---
+++ 13/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use wfm\interfaces\IGadget;
+use app\{BookProduct, NotebookProduct};
+
+//require_once 'phpmailer/src/PHPMailer.php';
+
+error_reporting(-1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+/*function autoloader1($class)
+{
+    $class = str_replace("\\", '/', $class);
+    $file = __DIR__ . "/{$class}.php";
+    if (file_exists($file)) {
+        require_once $file;
+    };
+};
+
+spl_autoload_register('autoloader1');*/
+
+function debug($data)
+{
+    echo '<pre>' . print_r($data,1) . '</pre>';
+}
+
+function offerCase(IGadget $product)
+{
+    echo "<p>Предлагаем чехол для гаджета {$product->getName()}</p>";
+}
+
+$book = new BookProduct('Три мушкетера', 20, 1000);
+$notebook = new NotebookProduct('Dell', 1000, 'Intel');
+
+offerCase($notebook);
+
+debug($book);
+
+//$mail = new \PHPMailer\PHPMailer\PHPMailer();
+//debug($mail);
+
+$mail = new \PHPMailer\PHPMailer\PHPMailer();
+debug($mail);
